edcbff8a6d7a4597d0686e9af70be0286b8c52b7
Waldman200036 <waldman200036@gmail.com>
2022-09-01 10:39:10 -0500

    1 file modified

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+
+/*Active Links*/
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
@@ -12,13 +14,19 @@ export default function Invoices() {
         }}
       >
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </Link>
+          <NavLink
+          style={({ isActive }) => {
+            return {
+              display: "block",
+              margin: "1rem 0",
+              color: isActive ? "red" : "",
+            };
+          }}
+          to={`/invoices/${invoice.number}`}
+          key={invoice.number}
+        >
+          {invoice.name}
+        </NavLink>
         ))}
       </nav>
       <Outlet />
